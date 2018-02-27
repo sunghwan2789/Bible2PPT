f672b299d5474fbe313cc9c6fc4a8580ab9e1ebe
Bang Sunghwan <sunghwan2789@gmail.com>
2018-02-28 08:37:11 +0900

    PPT 만들기 전에 모든 성경 구절 파싱하도록 변경

--- Bible2PPT/MainForm.cs
+++ Bible2PPT/MainForm.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -249,15 +250,13 @@ namespace Bible2PPT
                 }
 
                 var books = lstBooks.Tag as List<BibleBook>;
-                foreach (var query in
-                    txtKeyword.Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                        .Select(BibleQuery.ParseQuery))
+                foreach (var t in
+                    Regex.Replace(txtKeyword.Text.Trim(), @"\s+", "").Split()
+                        .Select(BibleQuery.ParseQuery)
+                        .Select(q => Tuple.Create(q, books.First(b => b.ShortTitle == q.BibleId))).ToList())
                 {
-                    var book = books.FirstOrDefault(i => i.ShortTitle == query.BibleId);
-                    if (book == null)
-                    {
-                        throw new IndexOutOfRangeException($@"""{query.BibleId}""에 해당하는 성경이 없습니다.");
-                    }
+                    var query = t.Item1;
+                    var book = t.Item2;
 
                     // TODO: book.chaptercount maybe null
                     foreach (var chapter in
