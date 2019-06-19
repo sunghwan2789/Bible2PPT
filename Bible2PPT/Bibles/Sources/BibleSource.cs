2b68f42df3b8813faf1775718d7a2ae377720115
Sunghwan Bang <sunghwan2789@gmail.com>
2019-06-19 18:51:45 +0900

    리펙토링

--- Bible2PPT/Bibles/Sources/BibleSource.cs
+++ Bible2PPT/Bibles/Sources/BibleSource.cs
@@ -50,193 +50,212 @@ namespace Bible2PPT.Bibles.Sources
             verse.ChapterId = chapter.Id;
         }
 
-        public List<Bible> GetBibles()
+
+        private void CacheBibles(List<Bible> bibles)
         {
-            List<Bible> bibles;
-
-            if (!AppConfig.Context.UseCache)
-            {
-                bibles = GetBiblesOnline();
-                bibles.ForEach(bible => LinkForeigns(bible));
-                return bibles;
-            }
-
-            using (var db = new BibleDb())
-            using (var cursor = db.Bibles)
-            {
-                cursor.SetCurrentIndex("SourceId");
-                cursor.FindRecords(MatchCriteria.EqualTo, Key.Compose(Id));
-                bibles = cursor.Cast<FieldCollection>().Select(BibleDb.MapEntity<Bible>).ToList();
-                if (bibles.Any())
-                {
-                    // ANNOYING LOOPS
-                    foreach (var bible in bibles)
-                    {
-                        LinkForeigns(bible);
-                    }
-                    return bibles;
-                }
-            }
-
-            bibles = GetBiblesOnline();
-
             using (var db = new BibleDb())
             using (var tx = db.Transaction)
             using (var cursor = db.Bibles)
             {
                 foreach (var bible in bibles)
                 {
-                    LinkForeigns(bible);
-
                     cursor.BeginEditForInsert();
                     BibleDb.MapEntity(cursor, bible);
                     cursor.AcceptChanges();
                 }
                 tx.Commit();
             }
-            return bibles;
         }
 
-        public List<Book> GetBooks(Bible bible)
+        private void CacheBooks(List<Book> books)
         {
-            List<Book> books;
-
-            if (!AppConfig.Context.UseCache)
-            {
-                books = GetBooksOnline(bible);
-                books.ForEach(book => LinkForeigns(book, bible));
-                return books;
-            }
-
-            using (var db = new BibleDb())
-            using (var cursor = db.Books)
-            {
-                cursor.SetCurrentIndex("BibleId");
-                cursor.FindRecords(MatchCriteria.EqualTo, Key.Compose(bible.Id));
-                books = cursor.Cast<FieldCollection>().Select(BibleDb.MapEntity<Book>).ToList();
-                if (books.Any())
-                {
-                    // ANNOYING LOOPS
-                    foreach (var book in books)
-                    {
-                        LinkForeigns(book, bible);
-                    }
-                    return books;
-                }
-            }
-
-            books = GetBooksOnline(bible);
-
             using (var db = new BibleDb())
             using (var tx = db.Transaction)
             using (var cursor = db.Books)
             {
                 foreach (var book in books)
                 {
-                    LinkForeigns(book, bible);
-
                     cursor.BeginEditForInsert();
                     BibleDb.MapEntity(cursor, book);
                     cursor.AcceptChanges();
                 }
                 tx.Commit();
             }
-            return books;
         }
 
-        public List<Chapter> GetChapters(Book book)
+        private void CacheChapters(List<Chapter> chapters)
         {
-            List<Chapter> chapters;
-
-            if (!AppConfig.Context.UseCache)
-            {
-                chapters = GetChaptersOnline(book);
-                chapters.ForEach(chapter => LinkForeigns(chapter, book));
-                return chapters;
-            }
-
-            using (var db = new BibleDb())
-            using (var cursor = db.Chapters)
-            {
-                cursor.SetCurrentIndex("BookId");
-                cursor.FindRecords(MatchCriteria.EqualTo, Key.Compose(book.Id));
-                chapters = cursor.Cast<FieldCollection>().Select(BibleDb.MapEntity<Chapter>).ToList();
-                if (chapters.Any())
-                {
-                    // ANNOYING LOOPS
-                    foreach (var chapter in chapters)
-                    {
-                        LinkForeigns(chapter, book);
-                    }
-                    return chapters;
-                }
-            }
-
-            chapters = GetChaptersOnline(book);
-
             using (var db = new BibleDb())
             using (var tx = db.Transaction)
             using (var cursor = db.Chapters)
             {
                 foreach (var chapter in chapters)
                 {
-                    LinkForeigns(chapter, book);
-
                     cursor.BeginEditForInsert();
                     BibleDb.MapEntity(cursor, chapter);
                     cursor.AcceptChanges();
                 }
                 tx.Commit();
             }
-            return chapters;
         }
 
-        public List<Verse> GetVerses(Chapter chapter)
+        private void CacheVerses(List<Verse> verses)
         {
-            List<Verse> verses;
-
-            if (!AppConfig.Context.UseCache)
-            {
-                verses = GetVersesOnline(chapter);
-                verses.ForEach(verse => LinkForeigns(verse, chapter));
-                return verses;
-            }
-
-            using (var db = new BibleDb())
-            using (var cursor = db.Verses)
-            {
-                cursor.SetCurrentIndex("ChapterId");
-                cursor.FindRecords(MatchCriteria.EqualTo, Key.Compose(chapter.Id));
-                verses = cursor.Cast<FieldCollection>().Select(BibleDb.MapEntity<Verse>).ToList();
-                if (verses.Any())
-                {
-                    // ANNOYING LOOPS
-                    foreach (var verse in verses)
-                    {
-                        LinkForeigns(verse, chapter);
-                    }
-                    return verses;
-                }
-            }
-
-            verses = GetVersesOnline(chapter);
-
             using (var db = new BibleDb())
             using (var tx = db.Transaction)
             using (var cursor = db.Verses)
             {
                 foreach (var verse in verses)
                 {
-                    LinkForeigns(verse, chapter);
-
                     cursor.BeginEditForInsert();
                     BibleDb.MapEntity(cursor, verse);
                     cursor.AcceptChanges();
                 }
                 tx.Commit();
             }
+        }
+
+        private List<Bible> GetBiblesCached()
+        {
+            using (var db = new BibleDb())
+            using (var cursor = db.Bibles)
+            {
+                cursor.SetCurrentIndex("SourceId");
+                cursor.FindRecords(MatchCriteria.EqualTo, Key.Compose(Id));
+                return cursor.Cast<FieldCollection>().Select(BibleDb.MapEntity<Bible>).ToList();
+            }
+        }
+
+        private List<Book> GetBooksCached(Bible bible)
+        {
+            using (var db = new BibleDb())
+            using (var cursor = db.Books)
+            {
+                cursor.SetCurrentIndex("BibleId");
+                cursor.FindRecords(MatchCriteria.EqualTo, Key.Compose(bible.Id));
+                return cursor.Cast<FieldCollection>().Select(BibleDb.MapEntity<Book>).ToList();
+            }
+        }
+
+        private List<Chapter> GetChaptersCached(Book book)
+        {
+            using (var db = new BibleDb())
+            using (var cursor = db.Chapters)
+            {
+                cursor.SetCurrentIndex("BookId");
+                cursor.FindRecords(MatchCriteria.EqualTo, Key.Compose(book.Id));
+                return cursor.Cast<FieldCollection>().Select(BibleDb.MapEntity<Chapter>).ToList();
+            }
+        }
+
+        private List<Verse> GetVersesCached(Chapter chapter)
+        {
+            using (var db = new BibleDb())
+            using (var cursor = db.Verses)
+            {
+                cursor.SetCurrentIndex("ChapterId");
+                cursor.FindRecords(MatchCriteria.EqualTo, Key.Compose(chapter.Id));
+                return cursor.Cast<FieldCollection>().Select(BibleDb.MapEntity<Verse>).ToList();
+            }
+        }
+
+
+        public List<Bible> GetBibles()
+        {
+            List<Bible> bibles;
+            // 오프라인 캐시 미사용 시 온라인 데이터 가져오기
+            if (!AppConfig.Context.UseCache)
+            {
+                bibles = GetBiblesOnline();
+                bibles.ForEach(LinkForeigns);
+                return bibles;
+            }
+            // 캐시가 있으면 사용
+            bibles = GetBiblesCached();
+            bibles.ForEach(LinkForeigns);
+            if (bibles.Any())
+            {
+                return bibles;
+            }
+            // 캐시가 없으면 온라인에서 가져와서 저장
+            bibles = GetBiblesOnline();
+            bibles.ForEach(LinkForeigns);
+            CacheBibles(bibles);
+            return bibles;
+        }
+
+        public List<Book> GetBooks(Bible bible)
+        {
+            List<Book> books;
+            // 오프라인 캐시 미사용 시 온라인 데이터 가져오기
+            if (!AppConfig.Context.UseCache)
+            {
+                books = GetBooksOnline(bible);
+                books.ForEach(book => LinkForeigns(book, bible));
+                return books;
+            }
+            // 캐시가 있으면 사용
+            books = GetBooksCached(bible);
+            books.ForEach(book => LinkForeigns(book, bible));
+            if (books.Any())
+            {
+                return books;
+            }
+            // 캐시가 없으면 온라인에서 가져와서 저장
+            books = GetBooksOnline(bible);
+            books.ForEach(book => LinkForeigns(book, bible));
+            CacheBooks(books);
+            return books;
+        }
+
+        public List<Chapter> GetChapters(Book book)
+        {
+            List<Chapter> chapters;
+            // 오프라인 캐시 미사용 시 온라인 데이터 가져오기
+            if (!AppConfig.Context.UseCache)
+            {
+                chapters = GetChaptersOnline(book);
+                chapters.ForEach(chapter => LinkForeigns(chapter, book));
+                return chapters;
+            }
+            // 캐시가 있으면 사용
+            chapters = GetChaptersCached(book);
+            chapters.ForEach(chapter => LinkForeigns(chapter, book));
+            if (chapters.Any())
+            {
+                return chapters;
+            }
+            // 캐시가 없으면 온라인에서 가져와서 저장
+            chapters = GetChaptersOnline(book);
+            chapters.ForEach(chapter => LinkForeigns(chapter, book));
+            CacheChapters(chapters);
+            return chapters;
+        }
+
+        public List<Verse> GetVerses(Chapter chapter)
+        {
+            List<Verse> verses;
+            // 오프라인 캐시 미사용 시 온라인 데이터 가져오기
+            if (!AppConfig.Context.UseCache)
+            {
+                verses = GetVersesOnline(chapter);
+                verses.ForEach(verse => LinkForeigns(verse, chapter));
+                return verses;
+            }
+            // 캐시가 있으면 사용
+            verses = GetVersesCached(chapter);
+            verses.ForEach(verse => LinkForeigns(verse, chapter));
+            if (verses.Any())
+            {
+                return verses;
+            }
+            // 캐시가 없으면 온라인에서 가져와서 저장
+            verses = GetVersesOnline(chapter);
+            verses.ForEach(verse => LinkForeigns(verse, chapter));
+            CacheVerses(verses);
             return verses;
         }
+
 
         public Task<List<Bible>> GetBiblesAsync() => Task.Factory.StartNew(GetBibles);
         public Task<List<Book>> GetBooksAsync(Bible bible) => Task.Factory.StartNew(() => GetBooks(bible));
