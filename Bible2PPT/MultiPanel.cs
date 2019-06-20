d2d3593a037d586237d250b62519f72813d15ce6
Sunghwan Bang <sunghwan2789@gmail.com>
2019-06-21 02:01:07 +0900

    MultiPanel 컴포넌트 추가

---
+++ Bible2PPT/MultiPanel.cs
@@ -0,0 +1,54 @@
+﻿// https://github.com/ennerperez/multipanelcontrol/blob/master/MultiPanelControl/MultiPanel.cs
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bible2PPT
+{
+    public class MultiPanel : Panel
+    {
+        public MultiPanelPage SelectedPage
+        {
+            get { return _selectedPage; }
+            set
+            {
+                _selectedPage = value;
+                if (_selectedPage != null)
+                {
+                    foreach (Control child in Controls)
+                    {
+                        if (object.ReferenceEquals(child, _selectedPage))
+                            child.Visible = true;
+                        else
+                            child.Visible = false;
+                    }
+                    OnSelectedPanelChanged(EventArgs.Empty);
+                }
+            }
+        }
+
+        protected virtual void OnSelectedPanelChanged(EventArgs e)
+        {
+            SelectedPanelChanged?.Invoke(this, e);
+        }
+
+        public event EventHandler SelectedPanelChanged;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+
+            using (SolidBrush br = new SolidBrush(BackColor))
+                g.FillRectangle(br, ClientRectangle);
+        }
+
+        protected override ControlCollection CreateControlsInstance()
+        {
+            return new MultiPanelPagesCollection(this);
+        }
+
+        private MultiPanelPage _selectedPage;
+    }
+}
