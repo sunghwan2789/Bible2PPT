28ac1d76004eee0c6ca0472fcaf9f90b78c3a598
Sunghwan Bang <sunghwan2789@gmail.com>
2019-06-30 02:54:42 +0900

    기본 컨트롤 추가

--- Bible2PPT/MainForm.Designer.cs
+++ Bible2PPT/MainForm.Designer.cs
@@ -88,8 +88,21 @@
             this.booksSearchTextBox = new System.Windows.Forms.TextBox();
             this.booksListView = new System.Windows.Forms.ListView();
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.templatesMultiPanelPage = new Bible2PPT.MultiPanelPage();
             this.historyMultiPanelPage = new Bible2PPT.MultiPanelPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.templatesMultiPanelPage = new Bible2PPT.MultiPanelPage();
             this.settingsMultiPanelPage = new Bible2PPT.MultiPanelPage();
             this.updateButton = new System.Windows.Forms.Button();
             this.cleanCacheButton = new System.Windows.Forms.Button();
@@ -152,6 +165,9 @@
             ((System.ComponentModel.ISupportInitialize)(booksSearchIconPictureBox)).BeginInit();
             versesTableLayoutPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(versesIconPictureBox)).BeginInit();
+            this.historyMultiPanelPage.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.settingsMultiPanelPage.SuspendLayout();
             navBottomFlowLayoutPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.biblesBindingSource)).BeginInit();
@@ -233,7 +249,6 @@
             this.historyNav.TabIndex = 2;
             this.toolTip.SetToolTip(this.historyNav, "PPT 제작 기록");
             this.historyNav.UseVisualStyleBackColor = false;
-            this.historyNav.Visible = false;
             this.historyNav.Click += new System.EventHandler(this.Nav_Click);
             // 
             // templatesNav
@@ -289,15 +304,15 @@
             // mainMultiPanel
             // 
             this.mainMultiPanel.Controls.Add(this.buildMultiPanelPage);
-            this.mainMultiPanel.Controls.Add(this.templatesMultiPanelPage);
             this.mainMultiPanel.Controls.Add(this.historyMultiPanelPage);
+            this.mainMultiPanel.Controls.Add(this.templatesMultiPanelPage);
             this.mainMultiPanel.Controls.Add(this.settingsMultiPanelPage);
             this.mainMultiPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainMultiPanel.Location = new System.Drawing.Point(48, 0);
             this.mainMultiPanel.Margin = new System.Windows.Forms.Padding(0);
             this.mainMultiPanel.Name = "mainMultiPanel";
             mainTableLayoutPanel.SetRowSpan(this.mainMultiPanel, 2);
-            this.mainMultiPanel.SelectedPage = this.buildMultiPanelPage;
+            this.mainMultiPanel.SelectedPage = this.historyMultiPanelPage;
             this.mainMultiPanel.Size = new System.Drawing.Size(461, 312);
             this.mainMultiPanel.TabIndex = 2;
             this.mainMultiPanel.SelectedPanelChanged += new System.EventHandler(this.MainMultiPanel_SelectedPanelChanged);
@@ -1029,6 +1044,124 @@
             versesLabel.TabIndex = 0;
             versesLabel.Text = "성경 구절";
             // 
+            // historyMultiPanelPage
+            // 
+            this.historyMultiPanelPage.Controls.Add(this.tableLayoutPanel1);
+            this.historyMultiPanelPage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.historyMultiPanelPage.Location = new System.Drawing.Point(0, 0);
+            this.historyMultiPanelPage.Name = "historyMultiPanelPage";
+            this.historyMultiPanelPage.Size = new System.Drawing.Size(461, 312);
+            this.historyMultiPanelPage.TabIndex = 0;
+            this.historyMultiPanelPage.Text = "PPT 제작 기록";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(13, 10, 13, 10);
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.9863F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.0137F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(461, 312);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column10,
+            this.Column8,
+            this.Column9,
+            this.Column11});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 45);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(435, 257);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "추가한 시간";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "사용한 성경";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "성경 구절";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "책 이름";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "약자";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "장 번호";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "장별로 나누기";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "결과";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "불러오기";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "열기";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "제거";
+            this.Column11.Name = "Column11";
+            this.Column11.ReadOnly = true;
+            // 
             // templatesMultiPanelPage
             // 
             this.templatesMultiPanelPage.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -1037,15 +1170,6 @@
             this.templatesMultiPanelPage.Size = new System.Drawing.Size(461, 312);
             this.templatesMultiPanelPage.TabIndex = 0;
             this.templatesMultiPanelPage.Text = "템플릿 관리";
-            // 
-            // historyMultiPanelPage
-            // 
-            this.historyMultiPanelPage.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.historyMultiPanelPage.Location = new System.Drawing.Point(0, 0);
-            this.historyMultiPanelPage.Name = "historyMultiPanelPage";
-            this.historyMultiPanelPage.Size = new System.Drawing.Size(461, 312);
-            this.historyMultiPanelPage.TabIndex = 0;
-            this.historyMultiPanelPage.Text = "PPT 제작 기록";
             // 
             // settingsMultiPanelPage
             // 
@@ -1161,6 +1285,9 @@
             versesTableLayoutPanel.ResumeLayout(false);
             versesTableLayoutPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(versesIconPictureBox)).EndInit();
+            this.historyMultiPanelPage.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.settingsMultiPanelPage.ResumeLayout(false);
             navBottomFlowLayoutPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.biblesBindingSource)).EndInit();
@@ -1207,6 +1334,19 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn biblesBibleDataGridViewColumn;
         private System.Windows.Forms.Button updateButton;
         private System.Windows.Forms.Button cleanCacheButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewButtonColumn Column8;
+        private System.Windows.Forms.DataGridViewButtonColumn Column9;
+        private System.Windows.Forms.DataGridViewButtonColumn Column11;
     }
 }
 
