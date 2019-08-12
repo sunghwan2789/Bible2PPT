c850ad517e5f72fce6aeb8fee64dbc4aa49d4398
Sunghwan Bang <sunghwan2789@gmail.com>
2019-08-12 11:16:02 +0900

    PostAndGetStringAsync로 리소스 정리

--- Bible2PPT/Bibles/Sources/GoodtvBible.cs
+++ Bible2PPT/Bibles/Sources/GoodtvBible.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Bible2PPT.Extensions;
 
 namespace Bible2PPT.Bibles.Sources
 {
@@ -36,26 +37,21 @@ namespace Bible2PPT.Bibles.Sources
 
         protected override async Task<List<Book>> GetBooksOnlineAsync(Bible bible)
         {
+            // TODO: WhenAll?
             var tasks = new[]
             {
-                client.PostAsync("/bible_otnt_exc.asp", new FormUrlEncodedContent(new[]
+                client.PostAndGetStringAsync("/bible_otnt_exc.asp", new FormUrlEncodedContent(new[]
                 {
                     new KeyValuePair<string, string>("bible_idx", "1"),
                     new KeyValuePair<string, string>("otnt", "1"),
                 })),
-                client.PostAsync("/bible_otnt_exc.asp", new FormUrlEncodedContent(new[]
+                client.PostAndGetStringAsync("/bible_otnt_exc.asp", new FormUrlEncodedContent(new[]
                 {
                     new KeyValuePair<string, string>("bible_idx", "1"),
                     new KeyValuePair<string, string>("otnt", "2"),
                 })),
             };
-            var data = "";
-            foreach (var task in tasks)
-            {
-                var response = await task;
-                response.EnsureSuccessStatusCode();
-                data += await response.Content.ReadAsStringAsync();
-            }
+            var data = string.Join("", await TaskEx.WhenAll(tasks));
             var matches = Regex.Matches(data, @"""idx"":(\d+).+?""bible_name"":""(.+?)"".+?""max_jang"":(\d+)");
             return matches.Cast<Match>().Select(i => new Book
             {
@@ -77,7 +73,7 @@ namespace Bible2PPT.Bibles.Sources
 
         protected override async Task<List<Verse>> GetVersesOnlineAsync(Chapter chapter)
         {
-            var response = await client.PostAsync("/bible.asp", new FormUrlEncodedContent(new[]
+            var data = await client.PostAndGetStringAsync("/bible.asp", new FormUrlEncodedContent(new[]
             {
                 new KeyValuePair<string, string>("bible_idx", chapter.Book.OnlineId),
                 new KeyValuePair<string, string>("jang_idx", chapter.OnlineId),
@@ -86,8 +82,6 @@ namespace Bible2PPT.Bibles.Sources
                 new KeyValuePair<string, string>("bible_version_3", "0"),
                 new KeyValuePair<string, string>("count", "1"),
             }));
-            response.EnsureSuccessStatusCode();
-            var data = await response.Content.ReadAsStringAsync();
             data = Regex.Match(data, @"<p id=""one_jang""><b>([\s\S]+?)</b></p>").Groups[1].Value;
             var matches = Regex.Matches(data, @"<b>(\d+).*?</b>(.*?)<br>");
             return matches.Cast<Match>().Select(i => new Verse
---
+++ Bible2PPT/Extensions/HttpClientExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bible2PPT.Extensions
+{
+    static class HttpClientExtension
+    {
+        public static Task<string> PostAndGetStringAsync(this HttpClient client, string requestUri, HttpContent content) =>
+            PostAndGetStringAsyncCore(client.PostAsync(requestUri, content));
+
+        private static async Task<string> PostAndGetStringAsyncCore(Task<HttpResponseMessage> postTask)
+        {
+            // Wait for the response message.
+            using (var responseMessage = await postTask.ConfigureAwait(false))
+            {
+                // Make sure it completed successfully.
+                responseMessage.EnsureSuccessStatusCode();
+
+                // Get the response content.
+                var c = responseMessage.Content;
+                if (c != null)
+                {
+                    return await c.ReadAsStringAsync().ConfigureAwait(false);
+                }
+
+                // No content to return.
+                return string.Empty;
+            }
+        }
+    }
+}
