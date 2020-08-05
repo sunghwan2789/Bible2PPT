64d643a363051ae710d8b287bc63ce02ef952e40
Sunghwan Bang <sunghwan2789@gmail.com>
2020-08-05 13:13:39 +0900

    refactor: Form 인스턴스 생성에 DI 사용 (#68)
    테스트를 위해 DI를 도입함.
    
    * chore: Dependency Injection 패키지 설치
    * refactor: Form 인스턴스 생성에 DI 사용

--- Bible2PPT/Program.cs
+++ Bible2PPT/Program.cs
@@ -1,30 +1,44 @@
 ﻿using System;
 using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualBasic.ApplicationServices;
 
 namespace Bible2PPT
 {
-    internal static class Program
+    static class Program
     {
-        class Startup : WindowsFormsApplicationBase
-        {
-            protected override void OnCreateSplashScreen()
-            {
-                SplashScreen = new SplashForm();
-            }
-
-            protected override void OnCreateMainForm()
-            {
-                MainForm = new MainForm();
-            }
-        }
+        static IServiceProvider ServiceProvider { get; set; }
 
         [STAThread]
         static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            ConfigureServices();
             new Startup().Run(args);
+        }
+
+        static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddTransient<SplashForm>();
+            services.AddTransient<MainForm>();
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        class Startup : WindowsFormsApplicationBase
+        {
+            protected override void OnCreateSplashScreen()
+            {
+                SplashScreen = ServiceProvider.GetService<SplashForm>();
+            }
+
+            protected override void OnCreateMainForm()
+            {
+                MainForm = ServiceProvider.GetRequiredService<MainForm>();
+            }
         }
     }
 }
