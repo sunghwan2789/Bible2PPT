d188f62f327780cc78a3d3b07c07649d101bec6f
Sunghwan Bang <sunghwan2789@gmail.com>
2022-02-04 13:44:33 +0900

    fix: 실행 파일이 네트워크 폴더에 있을 때 오류 없이 종료되는 현상 수정
    resolves #87

--- Bible2PPT/Controls/AssemblyIconForm.cs
+++ Bible2PPT/Controls/AssemblyIconForm.cs
@@ -1,13 +1,29 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Bible2PPT.Controls
 {
     public class AssemblyIconForm : Form
     {
+        private static readonly IntPtr _iconHandle;
+
+        static AssemblyIconForm()
+        {
+            unsafe
+            {
+                fixed (char* iconPath = Application.ExecutablePath)
+                {
+                    ushort iconId = 0;
+                    var iconHandle = Windows.Win32.PInvoke.ExtractAssociatedIcon(null, iconPath, &iconId);
+                    _iconHandle = iconHandle.DangerousGetHandle();
+                }
+            }
+        }
+
         public AssemblyIconForm()
         {
-            Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+            Icon = Icon.FromHandle(_iconHandle);
         }
     }
 }
